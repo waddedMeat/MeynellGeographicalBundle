b2c9fea8cb00288ea565e275d4197ef20a8e198d
moranjk <moranjk75@gmail.com>
2012-04-21 08:28:21 -0300

    added references to the data fixtures to allow for hooking into them from other fixtures

--- DataFixtures/ORM/LoadGeographicalData.php
+++ DataFixtures/ORM/LoadGeographicalData.php
@@ -18,47 +18,70 @@ class LoadGeographicalData extends AbstractFixture implements OrderedFixtureInte
         $manager->persist($country);
         $manager->flush();
 
+        $this->addReference('us-country', $country);
+
+
         $state = new State();
         $state->setCountry($country);
         $state->setName('Florida');
         $manager->persist($state);
         $manager->flush();
+        
+        $this->addReference('fl-state', $state);
+
 
         $county = new County();
         $county->setState($state);
         $county->setName('Orange');
         $manager->persist($county);
         $manager->flush();
+        
+        $this->addReference('orange-county', $county);
+
 
         $county = new County();
         $county->setState($state);
         $county->setName('Seminole');
         $manager->persist($county);
         $manager->flush();
+        
+        $this->addReference('seminole-county', $county);
+
 
         $county = new County();
         $county->setState($state);
         $county->setName('Hillsborough');
         $manager->persist($county);
         $manager->flush();
+        
+        $this->addReference('hillsborough-county', $county);
+
 
         $county = new County();
         $county->setState($state);
         $county->setName('Osceola');
         $manager->persist($county);
         $manager->flush();
+        
+        $this->addReference('osceola-county', $county);
+
 
         $county = new County();
         $county->setState($state);
         $county->setName('Polk');
         $manager->persist($county);
         $manager->flush();
+        
+        $this->addReference('polk-county', $county);
+
 
         $county = new County();
         $county->setState($state);
         $county->setName('Pinellas');
         $manager->persist($county);
         $manager->flush();
+        
+        $this->addReference('pinellas-county', $county);
     }
 
     public function getOrder()
